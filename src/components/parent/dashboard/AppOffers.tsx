ad44e09180087dba88a6a0878bb08c600528110e
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2025-11-12 03:23:44 +0100

    split parent dashboard into components

---
+++ src/components/parent/dashboard/AppOffers.tsx
@@ -0,0 +1,175 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useRef } from "react";
+import { Image, View } from "react-native";
+import Animated, {
+  runOnUI,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+import { Colors } from "@/src/constants/colors";
+import { ChartSpline } from "lucide-react-native";
+import Text from "../../ui/Text";
+
+const AppOffers = () => {
+  const scrollViewRef = useRef<Animated.ScrollView>(null);
+  const targetScrollX = useSharedValue(0);
+
+  const scrollToPosition = (position: number) => {
+    "worklet";
+    targetScrollX.value = withSpring(position);
+  };
+
+  useEffect(() => {
+    const autoScroll = () => {
+      setTimeout(() => {
+        runOnUI(() => {
+          scrollToPosition(315);
+        })();
+        scrollViewRef.current?.scrollTo({ x: 315, animated: true });
+      }, 3000);
+
+      setTimeout(() => {
+        runOnUI(() => {
+          scrollToPosition(630);
+        })();
+        scrollViewRef.current?.scrollTo({ x: 630, animated: true });
+      }, 6000);
+
+      setTimeout(() => {
+        runOnUI(() => {
+          scrollToPosition(0);
+        })();
+        scrollViewRef.current?.scrollTo({ x: 0, animated: true });
+      }, 9000);
+    };
+
+    autoScroll();
+
+    const interval = setInterval(autoScroll, 9000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <View className="mb-[25]">
+      <Text
+        className="text-[#4A90E2]"
+        style={{ fontSize: 20 }}
+        font="poppins-medium"
+      >
+        What The App Offers
+      </Text>
+      <Text className="text-[#4B5563]">Focus on what the app offers:</Text>
+
+      <Animated.ScrollView
+        ref={scrollViewRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="mt-[10] w-full"
+        scrollEventThrottle={16}
+      >
+        <View className="h-[130] w-[300] rounded-[12] overflow-hidden mr-[15]">
+          <LinearGradient
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+            className="flex-1 p-[10] px-[15] flex-row items-center justify-between"
+            colors={["#4A90E2", "#5DADEC"]}
+          >
+            <View className="w-[70%]">
+              <Text
+                className="text-white"
+                font="poppins-bold"
+                style={{ fontSize: 22 }}
+              >
+                Trust & Privacy
+              </Text>
+              <Text
+                className="text-white my-[2]"
+                style={{ fontSize: 14 }}
+              >
+                Your child chooses what to share. You get only what they allow.
+              </Text>
+            </View>
+            <View className="w-[25%]">
+              <View className="h-[70] w-[70] rounded-[35] bg-[#D9D9D975] items-center justify-center">
+                <Image
+                  className="h-[50] w-[50]"
+                  source={require("@/src/assets/images/project-images/padlock.png")}
+                />
+              </View>
+            </View>
+          </LinearGradient>
+        </View>
+
+        <View className="h-[130]  w-[300] rounded-[12] overflow-hidden mr-[15]">
+          <LinearGradient
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+            className="flex-1 p-[10] px-[15] flex-row items-center justify-between"
+            colors={["#4B2E83", "#9333EA"]}
+          >
+            <View className="w-[70%]">
+              <Text
+                className="text-white"
+                font="poppins-bold"
+                style={{ fontSize: 22 }}
+              >
+                Guided Insights
+              </Text>
+              <Text
+                className="text-white my-[2]"
+                style={{ fontSize: 14 }}
+              >
+                Simple trends and tips to understand your child better
+              </Text>
+            </View>
+            <View className="w-[25%]">
+              <View className="h-[70] w-[70] rounded-[35] bg-[#D9D9D975] items-center justify-center">
+                <ChartSpline
+                  size={25}
+                  color={Colors.light.white}
+                />
+              </View>
+            </View>
+          </LinearGradient>
+        </View>
+
+        <View className="h-[130]  w-[300] rounded-[12] overflow-hidden mr-[15]">
+          <LinearGradient
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+            className="flex-1 p-[10] px-[15] flex-row items-center justify-between"
+            colors={["#7F753E", "#E5D370"]}
+          >
+            <View className="w-[70%]">
+              <Text
+                className="text-white"
+                font="poppins-bold"
+                style={{ fontSize: 22 }}
+              >
+                Family Connection
+              </Text>
+              <Text
+                className="text-white my-[2]"
+                style={{ fontSize: 14 }}
+              >
+                Strengthen bonds through understanding and communication
+              </Text>
+            </View>
+            <View className="w-[25%]">
+              <View className="h-[70] w-[70] rounded-[35] bg-[#D9D9D975] items-center justify-center">
+                <Image
+                  className="h-[45] w-[45]"
+                  source={require("@/src/assets/images/project-images/family.png")}
+                />
+              </View>
+            </View>
+          </LinearGradient>
+        </View>
+      </Animated.ScrollView>
+    </View>
+  );
+};
+
+export default AppOffers;
---
+++ src/components/parent/dashboard/Tips.tsx
@@ -0,0 +1,93 @@
+import { Lightbulb } from "lucide-react-native";
+import { TouchableOpacity, View } from "react-native";
+
+import Text from "../../ui/Text";
+
+const Tips = () => {
+  return (
+    <View className="mb-[25]">
+      <Text
+        className="text-[#4A90E2]"
+        style={{ fontSize: 20 }}
+        font="poppins-medium"
+      >
+        Tip of the day
+      </Text>
+      <Text className="text-[#4B5563]">
+        Track, Understand & Support your child, emotionally and otherwise.
+      </Text>
+
+      <View className="my-3 gap-[15]">
+        <View className="h-[85] bg-[#9333EA8C] rounded-[12] p-[10] justify-center flex-row gap-[8] items-center">
+          <View className="bg-[#4A90E2] h-[48] w-[48] rounded-[12] items-center justify-center z-[10]">
+            <Lightbulb
+              size={30}
+              color={"#FFD700"}
+              fill={"#FFD700"}
+            />
+          </View>
+          <View className="absolute bottom-[8] h-[24] w-[32] bg-white/25 left-[55]  rounded-[100]"></View>
+          <View className="w-[80%]">
+            <Text
+              className="text-white text-lg"
+              font="poppins-medium"
+              numberOfLines={2}
+            >
+              Children open up more when they feel safe from judgment.
+            </Text>
+          </View>
+        </View>
+
+        <View className="h-[85] bg-[#6EE7B78C] rounded-[12] p-[10] justify-center flex-row gap-[8] items-center">
+          <View className="bg-[#4A90E2] h-[48] w-[48] rounded-[12] items-center justify-center z-[10]">
+            <Lightbulb
+              size={30}
+              color={"#FFD700"}
+              fill={"#FFD700"}
+            />
+          </View>
+          <View className="absolute bottom-[8] h-[24] w-[32] bg-white/25 left-[55]  rounded-[100]"></View>
+          <View className="w-[80%]">
+            <Text
+              className="text-lg"
+              numberOfLines={2}
+            >
+              Ask them ‘how did you feel’ instead of “what did you do”
+            </Text>
+          </View>
+        </View>
+
+        <View className="h-[85] bg-[#FFD93B8C] rounded-[12] p-[10] justify-center flex-row gap-[8] items-center">
+          <View className="bg-[#4A90E2] h-[48] w-[48] rounded-[12] items-center justify-center z-[10]">
+            <Lightbulb
+              size={30}
+              color={"#FFD700"}
+              fill={"#FFD700"}
+            />
+          </View>
+          <View className="absolute bottom-[8] h-[24] w-[32] bg-white/25 left-[55]  rounded-[100]"></View>
+          <View className="w-[80%]">
+            <Text
+              className="text-lg"
+              numberOfLines={2}
+            >
+              Always check insights about your child, daily, all insights are
+              available.
+            </Text>
+          </View>
+        </View>
+
+        <TouchableOpacity className="h-[48] rounded-[12] bg-[#4A90E2] items-center justify-center">
+          <Text
+            className="text-lg text-white"
+            font="poppins-medium"
+          >
+            Learn More
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Tips;
