f8a83386adf06430194f59bf86c3381804412a7b
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2025-11-19 18:31:40 +0100

    add no notification content to all-notification component

--- src/components/notifications/parent/tabs/All.tsx
+++ src/components/notifications/parent/tabs/All.tsx
@@ -1,11 +1,31 @@
 import { View } from "react-native";
 
 import Text from "@/src/components/ui/Text";
+import LottieView from "lottie-react-native";
 
 const AllNotifications = () => {
   return (
-    <View>
-      <Text>All Notifications</Text>
+    <View className="flex-1 items-center justify-center bg-[#B8E0FF]">
+      <LottieView
+        source={{
+          uri: "https://lottie.host/1685022b-f82e-425d-9525-0f9d0071f1c4/CgWT0Cz3mf.lottie",
+        }}
+        style={{
+          height: 50,
+          width: 50,
+        }}
+        autoPlay
+        loop
+      />
+      <Text
+        className="text-xl text-center"
+        font="poppins-medium"
+      >
+        You're all caught up
+      </Text>
+      <Text className="text-center">
+        We'll notify you when there's something new
+      </Text>
     </View>
   );
 };
