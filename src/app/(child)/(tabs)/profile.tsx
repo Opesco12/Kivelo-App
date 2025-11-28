e4e1cbacfe3e1198e8272362a75e82ab7bd35142
Emmanuel Oyeleke <oyelekemmanuel@gmail.com>
2025-11-28 02:13:25 +0100

    add settings, notifications and customize-profile files

--- src/app/(child)/(tabs)/profile.tsx
+++ src/app/(child)/(tabs)/profile.tsx
@@ -1,7 +1,7 @@
 import { Image } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
-import { Settings } from "lucide-react-native";
-import { View } from "react-native";
+import { Settings, Star } from "lucide-react-native";
+import { TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import BackButton from "@/src/components/ui/BackButton";
@@ -34,7 +34,7 @@ const Profile = () => {
             ></LinearGradient>
           </View>
 
-          <View className="flex-row absolute justify-between items-center w-[100%] bottom-[-30] px-[15]">
+          <View className="flex-row absolute justify-between items-center w-[100%] bottom-[-30] left-[15] px-[15]">
             <View className="relative">
               <Image
                 source={require("@/src/assets/images/project-images/avatar1.svg")}
@@ -78,9 +78,44 @@ const Profile = () => {
         </View>
         <View
           className="flex-1 mt-[70] bg-white px-[15]"
-          style={{ borderTopRightRadius: 70, borderTopLeftRadius: 70 }}
+          style={{ borderTopRightRadius: 60, borderTopLeftRadius: 60 }}
         >
-          <View className="px-[15] bg-white"></View>
+          <View
+            className="bg-white p-[15] rounded-[12] mt-[-30]"
+            style={{ elevation: 5 }}
+          >
+            <View className="flex-row justify-between items-center ">
+              <View className="">
+                <Text className="text-xl mb-[5]">Your Progress</Text>
+                <Text className="text-[#666666]">
+                  You played one game this week
+                </Text>
+              </View>
+
+              <View>
+                <View className="h-[38] w-[38]  bg-[#FFD93B] rounded-[19] items-center justify-center">
+                  <Star
+                    size={30}
+                    fill={"#FFF"}
+                    strokeWidth={0}
+                  />
+                </View>
+                <Text>Starter</Text>
+              </View>
+            </View>
+            <TouchableOpacity className="h-[45] rounded-[50] bg-[#4A90E2] items-center justify-center mt-[20]">
+              <Text className="text-white">Continue where you left off</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity className="h-[45] rounded-[50] bg-[#FB923C] items-center justify-center mt-[20]">
+            <Text
+              className="text-white text-2xl"
+              font="poppins-bold"
+            >
+              Leaderboard
+            </Text>
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </View>
